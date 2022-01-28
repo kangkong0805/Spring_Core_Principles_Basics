46352261019d0d8fc84078a9095e4d1850d31a7c
kangkong <kang3530554@gmail.com>
2022-01-28 23:56:52 +0900

    Start converting to spring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,22 +8,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     /* 역할에 따른 구현 정리 */
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());  // 회원 저장소 구현체를 생성자를 통해 주입(연결)
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(new MemoryMemberRepository(), discountPolicy());  // 회원 저장소 구현체와 할인 정책 구현체를 생성자를 통해 주입(연결)
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
