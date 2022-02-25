526f228ec99d286d77e1f888c7340e6f2e2667d8
kangkong <kang3530554@gmail.com>
2022-02-25 23:38:08 +0900

    Learn up to 중복 등록과 중돌 of the 컴포넌트 스캔 unit

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        // basePackages: 탐색할 패키지의 시작 위치를 지정
+        // basePackages: 탐색할 패키지의 시작 위치를 지정한다.
+        // basePackageClasses : 지정한 클래스의 패키지를 탐색 시작 위치로 지정한다.
+        // 지정 안했을 때 : '@ComponentScan'이 붙은 설정 정보 클래스의 패키지가 시작 위치로 된다.
         basePackages = "hello.core",
 
         // excludeFilters : 특정 조건을 만족하는 객체를 컴포넌트 스캔에서 제외시키는 설정
@@ -15,6 +17,6 @@ import org.springframework.context.annotation.FilterType;
                 classes = Configuration.class // @Configuration
         )
 )
-public class    AutoAppConfig {
+public class AutoAppConfig {
 
 }
---
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,37 @@
+package hello.core.scan.filter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.context.annotation.ComponentScan.*;
+
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void filterScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+
+        BeanA beanA = ac.getBean("BeanA", BeanA.class);
+        assertThat(beanA).isNotNull();
+
+        ac.getBean("BeanB", BeanB.class);
+        assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean("beanB", BeanB.class));
+
+    }
+
+    @Configuration
+    @ComponentScan(
+            includeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
+            excludeFilters = @Filter(/*type = FilterType.(기본값 = ANNOTATION)*/classes = MyIncludeComponent.class)
+    )
+    static class ComponentFilterAppConfig {
+
+    }
+}
